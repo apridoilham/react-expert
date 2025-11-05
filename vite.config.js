1e4172bcd34d7d65a5be89080fbe61bc3d517f6b
Aprido Ilham <apridoilham@idomac.local>
2025-11-05 15:53:07 +0700

    fix: downgrade to Vite 5 for Storybook 8 compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { resolve } from 'path' // Pastikan 'resolve' di-impor dari 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
